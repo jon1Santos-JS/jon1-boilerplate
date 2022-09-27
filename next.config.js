25154eeef13391413ca6a25df73c54cb25340eca
ubuntu <gupa581333@gmail.com>
2022-09-26 23:23:51 -0300

    first comit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const join = require('path').join;
 
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    sassOptions: {
+        includePaths: [join(__dirname, 'styles')],
+    },
+};
+
+module.exports = nextConfig;
