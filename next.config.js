56a9e0ba1e1927e56baf35347b569c965b522570
ubuntu <gupa581333@gmail.com>
2022-11-18 20:42:47 -0300

    npm install no readme

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-};
-
-module.exports = nextConfig;
